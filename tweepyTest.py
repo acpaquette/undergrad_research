23ff3f974968eeffa285c6c674111e4208871143
Adam Paquette <acp263@nau.edu>
2018-09-11 02:44:51 -0700

    Finished merging and modifying stream code

--- tweepyTest.py
+++ tweepyTest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 import tweepy
 import csv
 import configparser
@@ -8,21 +9,51 @@ import datetime
 
 class CustomStreamListener(tweepy.StreamListener):
 
-    def __init__(self, output_file):
+    def __init__(self, output_file, time):
         super().__init__()
-        self.output_file = output_file
+        self.default_output = os.path.split(os.path.splitext(output_file)[0])[-1]
+        self.base_path = os.path.split(output_file)[0]
+        self.output_file = self.update_output_file(self.default_output, self.base_path)
+        self.start_time = time
 
     def on_status(self, status):
         # Don't have to print to terminal, but nice
-        print(status.id, status.created_at, status.text.encode('utf-8'), status.user.id, status.user.screen_name, status.user.location, status.user.favourites_count)
+        # print(status.id, status.created_at, status.text.encode('utf-8'), status.user.id, status.user.screen_name, status.user.location, status.user.favourites_count)
         # Writes to csv
-        with open(get_filename(), 'a') as f:
+        with open(self.output_file, 'a') as f:
             writer = csv.writer(f)
             writer.writerow([status.id, status.created_at, status.text.encode('utf-8'), status.user.id, status.user.screen_name, status.user.location, status.user.favourites_count])
 
-        #if check_time returns false, exit tweet collection 'while' loop
-        if(check_time(datetime.datetime.now()) != True):
-            return False
+        # if check_time returns false, start a new collection file for the stream
+        if check_time(self.start_time, datetime.datetime.now()) != True:
+            CustomStreamListener.combine_time_files(self.output_file, self.base_path)
+            self.start_time = datetime.datetime.now()
+            self.output_file = self.update_output_file(self.default_output, self.base_path)
+
+        return True
+
+    @staticmethod
+    def update_output_file(output_file, base_path):
+        date = datetime.datetime.now()
+        output_file = '{}/{}_{}_{}_{}_{}_{}'.format(base_path, str(date.year), \
+                                                    str(date.month), str(date.day), \
+                                                    str(date.hour), str(date.minute), \
+                                                    output_file + '.csv')
+        print("Generating output:", output_file)
+
+        # Creation of output csv file
+        with open(output_file, 'w', encoding="utf8") as f:
+            writer = csv.writer(f)
+            writer.writerow(['TweetID', 'Timestamp', 'Tweet_Text_Content', 'UserID', 'User_Name', 'Country State City', 'Likes'])
+
+        return output_file
+
+    @staticmethod
+    def combine_time_files(file, base_path):
+        file_split = file.split('_')
+        composite_file = '{}_{}_{}_{}_composite.csv'.format(*file_split[0:4])
+        os.system('cat ' + file + ' >> ' + composite_file)
+        os.system('rm ' + file)
 
     # If error, keep streaming
     def on_error(self, status_code):
@@ -94,16 +125,7 @@ def parse_config(config_file):
 
     return consumer_key, consumer_secret, access_key, access_secret, output_file, keywords
 
-#if __name__ == "__main__":
-def __init__(time):
-
-    global master_time
-    master_time = time
-
-    #consumer key, consumer secret, access key, access secret.
-    args  = parse_args()
-
-def generate_twitter_stream(config_file):
+def generate_twitter_stream(config_file, start_time):
     '''
     Generates a twitter stream from a config file
 
@@ -123,16 +145,11 @@ def generate_twitter_stream(config_file):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
 
-    # Creation of output csv file
-    with open(get_filename(), 'w', encoding="utf8") as f:
-        writer = csv.writer(f)
-        writer.writerow(['TweetID', 'Timestamp', 'Tweet_Text_Content', 'UserID', 'User_Name', 'Country State City', 'Likes'])
-
     # Define streamingAPI
-    streaming_api = tweepy.streaming.Stream(auth, CustomStreamListener(output_file))
+    streaming_api = tweepy.streaming.Stream(auth, CustomStreamListener(output_file, start_time))
     return streaming_api, keywords
 
-def start_stream(config_file):
+def start_stream(config_file, start_time):
     '''
     Starts a stream from a config file
 
@@ -141,32 +158,29 @@ def start_stream(config_file):
     config_file : str
                   Path to a config file
     '''
-    streaming_api, keywords = generate_twitter_stream(config_file)
+    streaming_api, keywords = generate_twitter_stream(config_file, start_time)
     try:
         result = streaming_api.filter(track=keywords)
     except Exception as e:
         print("Stream Failed due to", e)
 
-if __name__ == "__main__":
-    args = parse_args()
-    print("Started", args.config_file)
+def check_time(start_time, tweet_time):
+    # gets time of tweet when collected
+    # gets master time from tweepyTimer
+    # compares master time against tweet time to see if time
+    # has past, change minute to hour, for hourly checks
 
-    start_stream(args.config_file)
-    # Call streamingAPI, which is stuck in while loop, not sure how to end w/o ctrl+c
-    streaming_api.filter(track=['car'])
-
-def check_time(tweet_time):
-    #print(tweet_time.minute)
-    #print(master_time.minute)
-    #gets time of tweet when collected
-    #gets master time from tweepyTimer
-    #compares master time against tweet time to see if time has past, change minute to hour, for hourly checks
-    if(master_time.minute == tweet_time.minute):
+    if start_time.hour == tweet_time.hour:
         return True
+
     return False
 
-def get_filename():
-    #concatenates file name together for dynamic naming based on master time
-    name = 'output_machine1_time'
-    ext = '.csv'
-    return name + str(master_time.minute) + ext
+def main():
+    start_time = datetime.datetime.now()
+
+    #consumer key, consumer secret, access key, access secret.
+    args  = parse_args()
+    start_stream(args.config_file, start_time)
+
+if __name__ == "__main__":
+    main()
