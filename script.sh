60d46544855b0df5041ee25eb072ea76cf2bc913
Adam Paquette <acp263@nau.edu>
2018-09-10 18:00:36 -0700

    Updated script and example config

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-python ./tweepyTest.py adam_config.ini &
-
-python ./tweepyTest.py dan_config.ini &
+python ./tweepyTest.py stream_config_1.ini &
+python ./tweepyTest.py stream_config_2.ini &
 
 echo Hello World!
