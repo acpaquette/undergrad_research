ab8640d9446434e9871dd3003f1599456f2a9f36
LtThunderDan <daw277@nau.edu>
2018-10-17 13:03:41 -0700

    nativevote18 keywrods

--- start_instagram.bash
+++ start_instagram.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-keyword=("NativeVote" "NativeVoices" "GOTNV" "Honorthetreaties" "NativeLivesMatter" \
-"NativesforBernie" "NotYourMascot" "MMIW" "Appropriation" "idlenomore" "nlm" \
-"nativelivesmatter" "notyourmascot" "notamascot" "notacostume" "eonm" "saveoakflat" \
-"protectoakflat" "dearnonnatives" "changethemascot" "changethename" "turtleisland" \
-"indigenous" "NDN" "N8V" "Colonialism" "decolonize" "indigenize" "IndianCountry" \
-"ihs" "indianhealthservices" "greatplains" "nativehealth" "flyingwhilebrown" \
-"freeleonardpeltier" "drivingwhilebrown" "nohonor" "nohonorinracism" "nativeyouth" \
-"nativevote" "apachestronghold" "TAIRP" "stopdisenrollment")
+keyword=("dosdinjiizhuh" "lymanhoffman" "akltgovbyronmallott" "jpeshlakai" \
+"chiefcaleensisk" "friends_of_lynn_decoite_" "votetupola" "kaialiikahele" \
+"kanielaing" "paulette_jordan" "shariceforcongress" "karenbrandendistrict2b" \
+"peggyflanagan" "kirsten4house" "mkmkunesh" "bessette4mt" "votekeatonsunchild" \
+"marvinwwaxjr" "deb4congressnm" "yvetteherrell" "georgenelouis" "nananow14" \
+"nmchoctaw" "devyn4ok" "carlyforok" "jehforhd35" "ashley4occ" "senpittman" \
+"apittman4hd99" "lindawade4ok" "tomcoleok04" "tuppertaylor" "moranforsenate" \
+"repangelaromero" "jme_urbannavajo" "nativevote" "sherepresents")
 
 # $1 is the bash profile to source for crontab
 source $1
